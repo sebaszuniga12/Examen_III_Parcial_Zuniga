ab60efc6d783375bfc6eb843b60505b4629b1a80
sebaszuniga12 <sebaszuni12@yahoo.com>
2021-09-10 13:02:43 -0500

    Mongo Template

---
+++ ExamenMongo/src/test/java/ec/edu/espe/distribuidas/examen/zuniga/mongo/MongoApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.distribuidas.examen.zuniga.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
